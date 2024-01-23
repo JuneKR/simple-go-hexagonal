b17e11f48bfa5af968dae4c39b90ae2ff9ffe167
Aekkaluk Panyacharoensri <d6600128@outsource.ktb.co.th>
2024-01-23 15:19:05 +0700

    [add] business logic of order

--- core/order_service.go
+++ core/order_service.go
@@ -1,5 +1,29 @@
 package core
 
+import "errors"
+
 type OrderService interface {
 	CreateOrder(order Order) error
+}
+
+type orderServiceImpl struct {
+	repo OrderRepository
+}
+
+// FUNC RETURN INSTANCE OF ORDER SERVICE
+func newOrderService(repo OrderRepository) OrderService {
+	return &orderServiceImpl{repo: repo}
+}
+
+// BUSINESS LOGIC
+func (s *orderServiceImpl) CreateOrder(order Order) error {
+	if order.Total <= 0 {
+		return errors.New("Total must be positive")
+	}
+
+	if err := s.repo.Save(order); err != nil {
+		return err
+	}
+
+	return nil
 }
